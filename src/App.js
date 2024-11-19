588136f5e26901bb06d77794f2c048552fde2c3a
Sathmi Rajapaksha <139479328+Sathmi1234@users.noreply.github.com>
2024-11-19 09:41:22 +0000

    Fontsize changing function

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import { useState } from 'react';
 import './App.css';
 import {students} from './assets/data/StudentsDb'
 import StudentTable from './components/StudentTable';
 
 function App() {
+  const [size,setSize]=useState(20);
+
+  
+
   return (
     <div className="App">
       <h1>Student Information Portal</h1>
       <hr/>
+      Font Size:
+      <button onClick={()=>setSize(16)}>S</button>
+      <button onClick={()=>setSize(20)}>M</button>
+      <button onClick={()=>setSize(24)}>L</button>
       <table width="100%">
         <tbody>
           <tr>
