f8b8099ed2bfbc552cb6f91d7d3f3a471925a7dd
Sathmi Rajapaksha <139479328+Sathmi1234@users.noreply.github.com>
2024-11-19 09:24:21 +0000

    Profile stucture created

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,13 +1,34 @@
 export default function Profile(){
     return(
         <div className="rightDiv">
-            <table>
+            <table border='1'>
                 <tr>
-                    <td colSpan={7}>
+                    <td rowSpan={8}>
                         <img className="profile"
-                            src={require('../assets/profilepic/'+props.student.profilePic)}
+                            //src={require('../assets/profilepic/'+props.student.profilePic)}
                             alt="Profile"/>
                     </td>
+                </tr>
+                <tr>
+                    <td>ID</td>
+                </tr>
+                <tr>
+                    <td>Full Name</td>
+                </tr>
+                <tr>
+                    <td>Age</td>
+                </tr>
+                <tr>
+                    <td>Course</td>
+                </tr>
+                <tr>
+                    <td>Gender</td>
+                </tr>
+                <tr>
+                    <td>Address</td>
+                </tr>
+                <tr>
+                    <td>Skill</td>
                 </tr>
             </table>
         </div>
