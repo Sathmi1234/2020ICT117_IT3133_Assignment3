8a61b19e044af7a6391d6e1692b64bebf4dee202
Sathmi Rajapaksha <139479328+Sathmi1234@users.noreply.github.com>
2024-11-19 09:04:23 +0000

    Table data added and width edited

--- src/components/StudentTable.js
+++ src/components/StudentTable.js
@@ -3,14 +3,26 @@ export default function StudentTable(props){
     return(
         <div className="outerDiv">
             <div className='leftDiv'>
-                <table>
-                    <tbody>
-                        <th>
+                <table border='1'>
+                    <thead>
+                        <tr>
                             <td>First Name</td>
                             <td>Last Name</td>
                             <td>Course</td>
                             <td>Country</td>
-                        </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            props.students.map(student=>
+                                <tr>
+                                    <td>{student.firstName}</td>
+                                    <td>{student.lastName}</td>
+                                    <td>{student.course}</td>
+                                    <td>{student.address.country}</td>
+                                </tr>
+                            )
+                        }
                     </tbody>
                 </table>
             </div>
