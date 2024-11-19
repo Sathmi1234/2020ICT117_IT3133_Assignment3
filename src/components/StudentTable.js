6034efed25f6065015d62b4182ec78713f56e6d8
Sathmi Rajapaksha <139479328+Sathmi1234@users.noreply.github.com>
2024-11-19 08:42:54 +0000

    Table Stucture created

--- src/components/StudentTable.js
+++ src/components/StudentTable.js
@@ -1,7 +1,19 @@
+import '../assets/css/compo.css'
 export default function StudentTable(props){
     return(
-        <div>
-
+        <div className="outerDiv">
+            <div className='leftDiv'>
+                <table>
+                    <tbody>
+                        <th>
+                            <td>First Name</td>
+                            <td>Last Name</td>
+                            <td>Course</td>
+                            <td>Country</td>
+                        </th>
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 }
